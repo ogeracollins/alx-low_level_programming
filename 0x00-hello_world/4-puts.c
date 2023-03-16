4234073bb395975da60ff3cf052aee8b4747b626
Collins Ogera <ogeracollins@gmail.com>
2023-03-16 07:16:47 -0700

    Using puts to display string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,8 @@
 
 int main(void)
 {
-	char sentence[] = "Programming is like building a multilingual puzzle";
+	char sentence[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(sentence);
 
 	return (0);
