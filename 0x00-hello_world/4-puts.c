c0d7b6c44284e75f71c2f414f17c9315db62c877
Collins Ogera <ogeracollins@gmail.com>
2023-03-16 07:14:06 -0700

    Using puts to display string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - Starting point of program
+*
+*Return: 0 as default
+*/
+
+int main(void)
+{
+	char sentence[] = "Programming is like building a multilingual puzzle";
+	puts(sentence);
+
+	return (0);
+}
