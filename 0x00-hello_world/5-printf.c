d6d3fed508ebd9aeb204931aa8bc9d74c6969fa3
Collins Ogera <ogeracollins@gmail.com>
2023-03-16 06:43:40 -0700

    Hello Prntf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - Starting point of program
+*
+*Return: 0 value is returned
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
