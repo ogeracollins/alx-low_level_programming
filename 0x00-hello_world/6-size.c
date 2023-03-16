04170dc0768c888b61821a420eda41598215f2b3
Collins Ogera <ogeracollins@gmail.com>
2023-03-16 07:06:39 -0700

    Size of primiteve datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 *main - Starting point of program
 *
@@ -8,11 +7,11 @@
 
 int main(void)
 {
-	printf("Size of a char: %c byte(s)\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of a int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n", (long) sizeof(int));
-	printf("Size of a long long int: %lld byte(s)\n", (long long) sizeof(int));
-	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
