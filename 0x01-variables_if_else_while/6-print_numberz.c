252955e733c8890ffa636d86f8238508dac2a879
Collins Ogera <ogeracollins@gmail.com>
2023-03-17 02:54:54 -0700

    Print integer using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Starting point of program
+*
+*Return: 0 as default value
+*/
+
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		putchar(a + '0');
+			a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
