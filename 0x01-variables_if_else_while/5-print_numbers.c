b0fde5be028f3644d4694a4807006b1f02a852b5
Collins Ogera <ogeracollins@gmail.com>
2023-03-17 02:49:46 -0700

    Numbers 0 - 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Starting point of program
+*
+*Return: 0 as default value
+*/
+
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+
+	return (0);
+}
