569d3f91fd17fe97313485f33c27cb04cb087a86
Collins Ogera <ogeracollins@gmail.com>
2023-03-17 02:05:30 -0700

    Print lowercase alphabet characters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Starting point of program
+*
+*Return: 0
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
