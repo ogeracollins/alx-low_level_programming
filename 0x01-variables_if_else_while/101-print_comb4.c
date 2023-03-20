99edad80df05cb4c7781c790d7fea33e974f7041
Collins Ogera <ogeracollins@gmail.com>
2023-03-19 23:36:49 -0700

    Print combination of three numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+/*
+*Author: Collins Ogera
+*Date: 3/20/2023
+*/
+
+#include <stdio.h>
+
+/**
+*main - Print combination of three digits
+*
+*Return: 0 as default value
+*/
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			for (c = b + 1; b < 10; b++)
+			{
+				putchar((a % 10) + '0');
+				putchar((b % 10) + '0');
+				putchar((c % 10) + '0');
+
+				if (a == 7 && b == 8 && c == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return ('0');
+}
