1da9569ced009ab9a12ad1d236ec59ac7bc2d2c9
Collins Ogera <ogeracollins@gmail.com>
2023-03-21 15:50:48 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.c"
+
+/**
+*main - Program to print putchar
+*
+*Return: 0 as default
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
