3aaf7764c280356d6068d30fb69eaf953abae085
Collins Ogera <ogeracollins@gmail.com>
2023-03-21 16:38:41 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*add - Function to add two numbers
+*@num1: Fist arguement
+*@num2: Second arguement
+*
+*Return: Summation result
+*/
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
