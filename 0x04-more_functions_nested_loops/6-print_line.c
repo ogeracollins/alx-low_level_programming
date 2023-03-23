d51399db2264dcc45d1979f459817be4b3defb77
Collins Ogera <ogeracollins@gmail.com>
2023-03-23 14:02:22 -0700

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_line - Prnit n lines using an underscore
+*@n: Number of times to draw _
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+		putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
