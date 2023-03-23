31351bb5edf61bba9db506abfc8d511b4ea89ac9
Collins Ogera <ogeracollins@gmail.com>
2023-03-23 14:10:11 -0700

    Print n line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,12 +9,10 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-		putchar('\n');
-	else
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 			_putchar('_');
-		_putchar('\n');
 	}
+	_putchat('\n');
 }
