9133c0f2a835716b4f6ad43c7fb35f7d3675cb09
Collins Ogera <ogeracollins@gmail.com>
2023-03-23 16:07:39 -0700

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*print_triangle - Function to print triangle
+*@size: Size of triangle
+*/
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = size - a; b > 0; b++)
+				_putchar(' ');
+
+			for (b = 0; b < a; b++)
+				_putchar('#');
+
+			if (a == size)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
