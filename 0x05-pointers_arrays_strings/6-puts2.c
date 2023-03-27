9a41d1d5cd23bb35e6d33a59d8d9034735177601
Collins Ogera <ogeracollins@gmail.com>
2023-03-27 11:53:03 -0700

    Print string chars

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*puts2 - Print characters of a string
+*@str: String to print
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i != '/0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
