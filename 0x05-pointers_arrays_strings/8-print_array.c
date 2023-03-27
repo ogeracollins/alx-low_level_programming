7ce41d35402e2eb391932a6fe9f9e92be0985783
Collins Ogera <ogeracollins@gmail.com>
2023-03-27 14:16:59 -0700

    Print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_array - Print n elements of an array
+*@a:Array
+*@n: Elements of array to be printed
+*/
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", a[x]);
+		if (x != n - 1)
+		{
+			printf(", ");
+		}
+	}
+}
