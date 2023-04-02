7308f842af158f68b52861a51a193195f09c22d5
Collins Ogera <ogeracollins@gmail.com>
2023-04-02 02:32:27 -0700

    Compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+/*
+*Author: Collins Ogera
+*Dare: 2/4/2023
+*/
+
+#include "main.h"
+#include <string.h>
+
+/**
+*_strcmp - Compare to strings
+*@s1: First stirng
+*@s2: Second string
+*
+*Return: 0 if equal, 1 if greater, -1 if less
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int s1Length, s2Length, i;
+
+	s1Length = strlen(s1);
+	s2Length = strlen(s2);
+
+	if (s1Length == s2Length)
+	{
+		for (i = 0; i < s1Length; i++)
+		{
+			if (*(s1 + i) == *(s2 + i))
+				return (0);
+			else
+				return (-1);
+		}
+	}
+	else if (s1Length > s2Length)
+		return (1);
+	else
+		return (-1);
+}
