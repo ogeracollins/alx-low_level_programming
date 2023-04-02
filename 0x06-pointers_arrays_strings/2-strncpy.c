e72706a3e5070d5af21366cb0577ec04239d8a8f
Collins Ogera <ogeracollins@gmail.com>
2023-04-02 00:43:21 -0700

    Copy string to length n

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+/*
+ *Author: Collins Ogera
+ *Date: 2/4/2023
+ */
+
+#include "main.h"
+
+/**
+*_strncpy - Copy string to length n
+*@dest: Destination
+*@src: Sourcs string
+*@n: Max length to copy
+*
+*Return: Dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && *(src + i) != '\0'; i++)
+		*(dest + i) = *(src + i);
+	*(dest + i) = '\0';
+
+	return (dest);
+}
