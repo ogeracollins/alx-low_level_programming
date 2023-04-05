8350ce8461b2c66a6fc13cd7d1b6506b67683316
Collins Ogera <ogeracollins@gmail.com>
2023-04-05 03:37:22 -0700

    Locate a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+/*
+*Author: Collins Ogera
+*Date: 4/5/2023
+*/
+#include "main.h"
+
+/**
+*_strchr - Locate character in string
+*@s: String
+*@c: Character to check
+*
+*Return: Pointer to first occurrence of c
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
