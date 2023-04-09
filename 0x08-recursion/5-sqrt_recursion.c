99b46d8373aebffe9672212cb101d8e7027d34aa
Collins Ogera <ogeracollins@gmail.com>
2023-04-09 03:54:12 -0700

    SquareRoot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,44 @@
+/*
+*Author: Collins Ogera
+*Date: 4/9/2023
+*/
+#include "main.h"
+/**
+*squareRoot - Calculate Square Root
+*@initial: 2
+*@end: Param 2
+*@m: Value to find sqrt
+*
+*Return: Squareroot of m
+*/
+
+int squareRoot(int initial, int end, int m)
+{
+	int middle;
+
+	if (end >= initial)
+	{
+		middle = initial + (end - initial) / 2;
+		if (middle * middle == m)
+			return (middle);
+		if (middle * middle > m)
+			return (squareRoot(initial, middle - 1, m));
+		if (middle * middle < m)
+			return (squareRoot(middle + 1, emd, m));
+	}
+}
+
+/**
+*_sqrt_recursion - Get squareroot using recursion
+*@n: Number to obtain sqrt
+*
+*Return: Squareroot
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (squareRoot(2, n, n));
+}
