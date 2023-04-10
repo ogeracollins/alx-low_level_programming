85002341f294fba132f5b56bb54348cfcb376d97
Collins Ogera <ogeracollins@gmail.com>
2023-04-10 02:31:33 -0700

    Agruements count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - Print number of arguements
+*@argc: Argument count
+*@argv: Arguement vector
+*
+*Return: 0 on success
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
