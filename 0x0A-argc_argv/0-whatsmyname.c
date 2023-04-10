25c3e88b515359073fcafa8468b860001142e007
Collins Ogera <ogeracollins@gmail.com>
2023-04-10 02:10:06 -0700

    What's my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - Program to print its name
+*@argc: Argument count
+*@argv: Argument point
+*
+*Return: 0 on success
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
