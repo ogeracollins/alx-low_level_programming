ec2c76b4333bc42edfb452af1dd5cb8b7b58b25b
Collins Ogera <ogeracollins@gmail.com>
2023-04-11 03:30:24 -0700

    String duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+/*
+*Author: Collins Ogera
+*Date: 4/11/2023
+*/
+#include <stdlib.h>
+
+/**
+*_strdup - Duplicate string using malloc
+*@str: String to duplicate
+*
+*Return: Pointer to dest
+*/
+
+char *_strdup(char *str)
+{
+	char *dest;
+	int len, i;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{}
+
+	dest = malloc(sizeof(str) * len);
+
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		dest[i] = str[i];
+	dest[i] = '\0';
+	return (dest);
+}
