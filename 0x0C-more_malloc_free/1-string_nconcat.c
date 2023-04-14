0c76dcb41b29a0931be2c729a0adeeb945f21a6d
Collins Ogera <ogeracollins@gmail.com>
2023-04-14 13:10:08 -0700

    Concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+/*
+*Author: Collins Ogera
+*Date: 4/14/2021
+*/
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*string_nconcat - Concat two string
+*@s1: String one
+*@s2: String two
+*@n: S2 bytes to concat
+*
+*Return: Pointer to new string
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j, s1len = 0, s2len = 0;
+	char *res;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		s1len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		s2len++;
+	if (n > s2len)
+		n = s2len;
+
+	res = malloc(sizeof(char) * (s1len + n + 1));
+	if (res == NULL)
+		return (NULL);
+
+	for (j = 0; j < s1len; j++)
+		*(res + j) = *(s1 + j);
+	for (i = 0; i < n; i++)
+		*(res + s1len + i) = *(s2 + i);
+	*(res + s1len + n) = '\0';
+
+	return (res);
+}
