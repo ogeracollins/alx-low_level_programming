c92e82fab4e5549ea54d1bf013916f66c3efae8f
Collins Ogera <ogeracollins@gmail.com>
2023-04-14 13:40:26 -0700

    Calloc Malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,13 +15,16 @@
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	int *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * sizeof(size));
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	return (ptr);
+	for (i = 0; i < nmemb * size; i++)
+		*(ptr + i) = 0;
+	return ((void *)ptr);
 }
