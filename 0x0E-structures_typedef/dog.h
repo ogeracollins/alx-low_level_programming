25a429e0df30d77b9846b5e81f001e78f48b6118
Collins Ogera <ogeracollins@gmail.com>
2023-04-17 02:52:28 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - Dog properties
+ *@name: Name of the dog
+ *@age: Age of the dog
+ *@owner: Who owns it
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
