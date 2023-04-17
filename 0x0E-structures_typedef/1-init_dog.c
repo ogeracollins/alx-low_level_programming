d669db40aa28aa201cca0fa795e614340f4877d5
Collins Ogera <ogeracollins@gmail.com>
2023-04-17 03:14:58 -0700

    Init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ *init_dog - Initialize struct dog
+ *@d: Pointer to struct
+ *
+ *@name: Name of the dog
+ *@age: Age of dog
+ *@owner: Dog's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
