2740944fae4ffb649888a2aa941b5e5a09421cb8
Collins Ogera <ogeracollins@gmail.com>
2023-04-20 03:00:15 -0700

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,11 +21,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (i != (n - 1))
-		{
-			if (separator != NULL)
-				printf("%c ", *separator);
-		}
+		if (i != (n - 1) && separator != NULL)
+			printf("%c ", *separator);
 	}
 	printf("\n");
 }
