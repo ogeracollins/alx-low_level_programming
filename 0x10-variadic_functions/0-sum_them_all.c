fd2a197eeabb7a197e10fc4a2ca79d17fd08fdb3
Collins Ogera <ogeracollins@gmail.com>
2023-04-20 01:22:19 -0700

    Ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ *sum_them_all - Sum arguments of variadic func
+ *@n: Number of arg expected
+ *
+ *Return: Summation
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int value;
+	int sum = 0;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(args, int);
+		sum += value;
+	}
+	va_end(args);
+
+	return (sum);
+}
