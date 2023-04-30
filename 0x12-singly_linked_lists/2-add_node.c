5ec5cbe796cfd98de79659d6de124b07d57318c0
Collins Ogera <ogeracollins@gmail.com>
2023-04-30 13:45:47 -0700

    Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+/*
+ *Author: Collins Ogera
+ *Date: 4/30/2023
+ */
+
+#include "lists.h"
+#include <string.h>
+/**
+ *add_node - Add a new node to list;
+ *@head: Start of the node
+ *@str: String to add
+ *
+ *Return: Address of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int i, len;
+	char *newStr;
+	list_t *new_node;
+
+	if (str == NULL || *head == NULL)
+		return (NULL);
+	len = strlen(str);
+
+	newStr = malloc((len + 1) * sizeof(char));
+	if (newStr == NULL)
+		return (NULL);
+	for (i = 0; i < len + 1; i++)
+		newStr[i] = str[i];
+	newStr[len + 1] = '\0';
+
+	new_node = *head;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = newStr;
+	new_node->len = len;
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
