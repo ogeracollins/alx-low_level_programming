192ac94892382e165e4a2eafaa82c9ca581a03de
Collins Ogera <ogeracollins@gmail.com>
2023-04-30 03:51:36 -0700

    Updated header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,4 +1,4 @@
-#ifndef LIST_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdio.h>
@@ -13,9 +13,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 int _putchar(char c);
@@ -25,4 +25,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /*LISTS_H*/
