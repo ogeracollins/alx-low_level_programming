ae4b436ef323cebe6b718a0d03c2304bf9106b32
Collins Ogera <ogeracollins@gmail.com>
2023-05-02 15:11:42 -0700

    Free more lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - Free a list
+ *@head: Pointer to pointer to head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t temp;
+
+	if (*head == NULL)
+	{}
+	if (*head != NULL)
+	{
+		temp = *head;
+		*head = *head->next;
+		free(temp);
+	}
+}
