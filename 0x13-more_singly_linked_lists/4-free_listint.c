c2bca4bb492840a0beae653260f9714b3363203d
Collins Ogera <ogeracollins@gmail.com>
2023-05-02 14:56:30 -0700

    Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+/*
+ *Author: Collins Ogera
+ *Date: 3/5/2023
+ */
+
+#include "lists.h"
+
+/**
+ *free_listint - Free lists
+ *@head: Pointer to list head
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	if (head == NULL)
+	{}
+	while (head->next != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
