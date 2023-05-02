37301340033db59ec5e20a4439689b8cb0e0b5f6
Collins Ogera <ogeracollins@gmail.com>
2023-05-02 15:14:37 -0700

    Free more lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,14 +6,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t temp;
+	listint_t *temp;
 
-	if (*head == NULL)
-	{}
-	if (*head != NULL)
+	while (*head != NULL)
 	{
 		temp = *head;
 		*head = *head->next;
 		free(temp);
 	}
+	*head = NULL;
 }
