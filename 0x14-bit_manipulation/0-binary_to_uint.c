dc96b73c90ff53a5fcecbdcec746e2f8e43a9ba1
Collins Ogera <ogeracollins@gmail.com>
2023-05-04 12:55:06 -0700

    Binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,6 +2,20 @@
 #include <math.h>
 
 /**
+ *Power - Rase a base to power
+ *@a: Base
+ *@b: Exponent
+ *
+ *Return: a ^ b
+ */
+int Power(int a, int b)
+{
+	int power = 1, i;
+	for(i = 1; i <= b; ++i)
+		power = power * a;
+	return (power);
+}
+/**
 *binary_to_uint - Convert bin to int
 *@b: Binary number
 *
@@ -22,7 +36,7 @@ unsigned int binary_to_uint(const char *b)
 		if (b[c] != '0' && b[c] != '1')
 			return (0);
 		if (b[c] == '1')
-			res += pow(2, len);
+			res += Power(2, len);
 		len--;
 	}
 	return (res);
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,7 @@
 #include <math.h>
 
 int _putchar(char c);
+int Power(int a, int b);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
