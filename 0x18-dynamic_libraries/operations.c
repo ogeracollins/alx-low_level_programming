0aeb4485f1c5d6094efb5bba7cffea6a5a273472
Collins Ogera <ogeracollins@gmail.com>
2023-06-20 04:19:49 -0700

    Call C from python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+/**
+ *add - Summation of two numbers
+ *@a: First number
+ *@b: Second number
+ *
+ *Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *sub - Subtract
+ *@a: First number
+ *@b: Second number
+ *
+ *Return: a - b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *mul - Mulitply two numbers
+ *@a: First number
+ *@b: Second number
+ *
+ *Return: a - b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *div - Division
+ *@a: First number
+ *@b: Second number
+ *
+ *Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *mod - Modulus of a number
+ *@a: First number
+ *@b: Second number
+ *
+ *Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
